75b76d61142c3d013f22aeabe5a0b0f7adb4a179
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2022-12-14 15:10:31 -0600

    Used NASA api to collect date, exp, hdurl & title = dummyData

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 
+const dummyData = {
+  date: "2022-12-14",
+  explanation: "Why is the Moon so dusty? On Earth, rocks are weathered by wind and water, creating soil and sand. On the Moon, the history of constant micrometeorite bombardment has blasted away at the rocky surface creating a layer of powdery lunar soil or regolith.  For the Apollo astronauts and their equipment, the pervasive, fine, gritty dust was definitely a problem",
+  hdurl: "https://apod.nasa.gov/apod/image/2212/AS17-137-20979.jpg",
+  title: "Lunar Dust and Duct Tape"
+}
+
 function App() {
+  const [data, setData] = useState(dummyData);
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <p>
@@ -12,7 +29,5 @@ function App() {
     </div>
   );
 }
-
-//https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
 
 export default App;
