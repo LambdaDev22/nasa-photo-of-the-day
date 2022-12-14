20a052f27595581e56da7223c9eaaa9663f06673
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2022-12-14 15:13:37 -0600

    Created components NasaPhoto and imported dummy data

---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const dummyData = {
+    date: "2022-12-14",
+    explanation: "Why is the Moon so dusty? On Earth, rocks are weathered by wind and water, creating soil and sand. On the Moon, the history of constant micrometeorite bombardment has blasted away at the rocky surface creating a layer of powdery lunar soil or regolith.  For the Apollo astronauts and their equipment, the pervasive, fine, gritty dust was definitely a problem",
+    hdurl: "https://apod.nasa.gov/apod/image/2212/AS17-137-20979.jpg",
+    title: "Lunar Dust and Duct Tape"
+  }
+
+const NasaPhoto = (props) => {
+    return (
+
+    )
+}
+
+export default NasaPhoto;
